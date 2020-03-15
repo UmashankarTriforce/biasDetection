c5061c91ddf99642b37bd0f403e907a2f9edde63
UmashankarTriforce <umashankar.sivakumar@live.in>
2020-03-15 22:46:05 +0530

    bug fixes

--- parallelCNDP/src/graph.h
+++ parallelCNDP/src/graph.h
@@ -18,12 +18,13 @@ public:
 template <typename T>::Graph<T>::Graph(std::vector< std::vector<T> > host) {
 	arr.push_back(host.size());
 	int count = 0;
+	arr.push_back(count);
 	for (auto i = 0; i < arr[0]; ++i) {
-		arr.push_back(count);
 		count += host[i].size();
+		arr.push_back(count);
 	}
 	for (auto i = 0; i < arr[0]; ++i) {
-		arr.insert(arr.begin() + arr[0] + 1 + arr[i + 1], host[i].begin(), host[i].end());
+		arr.insert(arr.begin() + arr[0] + 2 + arr[i + 1], host[i].begin(), host[i].end());
 	}
 
 	Graph<T>::printGraph(arr);
