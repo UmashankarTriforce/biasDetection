8e9d2f390b42ad3c40bf0594d7bc083e0ac62bc8
UmashankarTriforce <umashankarsivakumar@gmail.com>
2020-03-16 11:40:01 +0530

    fixed bugs

--- include/graph.hpp
+++ include/graph.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 #include <CL/cl.hpp>
 
@@ -14,3 +15,28 @@ public:
 	Graph(std::vector< std::vector<T> > host); //Init Graph
 	void printGraph(); // Print Graph
 };
+
+template <typename T>::Graph<T>::Graph(std::vector< std::vector<T> > host) {
+
+	//Initialize the Graph in the format of <number of nodes>,<cumulative number of edges>,<edges>
+
+	arr.push_back(host.size());
+	int count = 0;
+	arr.push_back(count);
+	for (auto i = 0; i < arr[0]; ++i) {
+		count += host[i].size();
+		arr.push_back(count);
+	}
+	for (auto i = 0; i < arr[0]; ++i) {
+		arr.insert(arr.begin() + arr[0] + 2 + arr[i + 1], host[i].begin(), host[i].end());
+	}
+}
+
+template <typename T> void::Graph<T>::printGraph() {
+
+	// Print the Graph
+
+	for (auto i = 0; i < arr.size(); ++i) {
+		std::cout << arr[i] << std::endl;
+	}
+}
