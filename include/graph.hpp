7e8bba00f4f94957deccda8d6fbc6668edabfbf4
UmashankarTriforce <umashankarsivakumar@gmail.com>
2020-03-16 15:11:04 +0530

    removed unecessary code

--- include/graph.hpp
+++ include/graph.hpp
@@ -16,26 +16,41 @@ private:
 	//OpenCL types
 	std::vector<cl::Platform> all_platforms;
 	std::vector<cl::Device> all_devices;
-	cl::Platform default_platform;
 	cl::Device default_device;
 
 	//Member functions
-	void initOpenCL(); // initialize opencl context
 	void sendDataToDevice(); //send the data from host to device
 
 public:
 
 	//Member functions
-	Graph(std::vector< std::vector<T> > host); //initialize Graph
-	void printGraph(); // Print Graph
+	Graph(std::vector< std::vector<T> > host, int platformID = 0, int deviceID = 0); //initialize Graph
+	void printGraph(); // print graph
 
 };
 
-template <typename T>::Graph<T>::Graph(std::vector< std::vector<T> > host) {
+template <typename T>::Graph<T>::Graph(std::vector< std::vector<T> > host, int platformID, int deviceID) {
+
+	//Initialize and get all devices with OpenCL 
+
+	cl::Platform::get(&all_platforms);
+	if (all_platforms.size() == 0) {
+		std::cout << " No platforms found. Check OpenCL installation!" << std::endl;
+		exit(1);
+	}
+
+	all_platforms[platformID].getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+	if (all_devices.size() == 0) {
+		std::cout << " No devices found. Check OpenCL installation!" << std::endl;
+		exit(1);
+	}
+
+	std::cout << "Using platform: " << all_platforms[platformID].getInfo<CL_PLATFORM_NAME>() << std::endl;
+	std::cout << "Using device: " << all_devices[deviceID].getInfo<CL_DEVICE_NAME>() << std::endl;
+
+	default_device = all_devices[deviceID];
 
 	//Initialize the Graph in the format of <number of nodes>,<cumulative number of edges>,<edges>
-
-	initOpenCL();
 
 	hostArr.push_back(host.size());
 	int count = 0;
@@ -47,6 +62,7 @@ template <typename T>::Graph<T>::Graph(std::vector< std::vector<T> > host) {
 	for (auto i = 0; i < hostArr[0]; ++i) {
 		hostArr.insert(hostArr.begin() + hostArr[0] + 2 + hostArr[i + 1], host[i].begin(), host[i].end());
 	}
+
 }
 
 template <typename T> void::Graph<T>::printGraph() {
@@ -56,27 +72,4 @@ template <typename T> void::Graph<T>::printGraph() {
 	for (auto i = 0; i < hostArr.size(); ++i) {
 		std::cout << hostArr[i] << std::endl;
 	}
-}
-
-template <typename T> void::Graph<T>::initOpenCL() {
-
-	//Initialize and get all devices with OpenCL 
-
-	cl::Platform::get(&all_platforms);
-	if (all_platforms.size() == 0) {
-		std::cout << " No platforms found. Check OpenCL installation!\n";
-		exit(1);
-	}
-
-	default_platform = all_platforms[0];
-	std::cout << "Using platform: " << default_platform.getInfo<CL_PLATFORM_NAME>() << "\n";
-
-	//get default device of the default platform
-	default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
-	if (all_devices.size() == 0) {
-		std::cout << " No devices found. Check OpenCL installation!\n";
-		exit(1);
-	}
-	default_device = all_devices[0];
-	std::cout << "Using device: " << default_device.getInfo<CL_DEVICE_NAME>() << "\n";
 }
