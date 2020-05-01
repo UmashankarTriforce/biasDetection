77971f467c94ff7de89c5176da815e9f7cd57ac1
Thejas-bhat <thejasbhat.6@gmail.com>
2020-05-01 19:52:08 +0530

    taking the network graph as an input for the cndp algo

--- apps/testFile.cpp
+++ apps/testFile.cpp
@@ -1,6 +1,12 @@
+#include <boost/algorithm/string.hpp>
+#include <fstream>
+#include <iostream>
 #include <graph.hpp>
+#include <unordered_map>
 
-int main() {
+using namespace std;
+
+void test_on_dummy() {
 	std::vector<std::vector <int> > test{
 		{3, 1},
 		{0, 2, 3},
@@ -10,6 +16,40 @@ int main() {
 	};
 
 	Graph<int> Test(test);
-	return 0;
+	
+}
 
+int main(){
+	//testing on the dummy graph;
+	// test_on_dummy();
+    ifstream myfile("/home/thejas/Sem 6/HP/biasDetection/scripts/test.adjlist");
+    string line;
+    vector<vector<int>> adjlist;
+    unordered_map<float, int> map;
+    
+    if(myfile.is_open()){
+        vector<string> result;
+		int k = 0;
+        while(getline(myfile, line)){
+            vector<int> temp;
+            boost::split(result, line, boost::is_any_of(","));
+            for(int i = 0; i < result.size(); i++){
+				float key = stof(result[i]);
+				if(map.find(key) == map.end()){
+					map[key] = k;
+					temp.push_back(k++);
+				}
+				else{
+					temp.push_back(map[key]);
+				}
+            }
+            adjlist.push_back(temp);
+        }
+    }
+    else{
+        cout << "not able to open the file please check the input path in testFile.cpp file" << endl;
+    }
+
+    Graph<int> Test(adjlist);
+    return 0;
 }
